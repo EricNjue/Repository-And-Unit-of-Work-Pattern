ef238a482c1a6fc42ca6fe667b9cbc5f14a6f92c
EricNjue <eric.gitonga38@gmail.com>
2017-04-02 22:21:17 +0300

    How to use Repository & Unit Of Work Pattern in order to make application testability using UnitTests

--- Repository_UnitOfWorkPattern/IBooksRepository.cs
+++ Repository_UnitOfWorkPattern/IBooksRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository_UnitOfWorkPattern
 {
-    public class Class1
+    public interface IBooksRepository
     {
     }
 }
---
+++ Repository_UnitOfWorkPattern/MockModels/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RUWP.DataAccess.MockModels
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string ISBN { get; set; }
+    }
+}
